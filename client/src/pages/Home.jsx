98d6ff1e258e6b4f3cd7b8798ca8cda61f0896a8
Jallipalli Snigdha <165471110+Snig10dha@users.noreply.github.com>
2025-06-30 19:36:04 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Testimonials from '../components/Testimonials'
